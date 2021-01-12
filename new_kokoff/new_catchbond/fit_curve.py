64aaa9d9c9e288b5529ab96b556453a620ed666c
yuikaueda <yuika428@gmail.com>
2021-01-12 18:22:31 +0900

    111n

--- new_kokoff/new_catchbond/fit_curve.py
+++ new_kokoff/new_catchbond/fit_curve.py
@@ -3,7 +3,7 @@ import numpy as np
 #import seaborn as sns
 import matplotlib.pyplot as plt
 
-data1 = np.loadtxt('kcon_20_num_l.dat')
+data1 = np.loadtxt('kcon_20_num_s.dat')
 
 def fit(x,K,T):
     return K*(1-np.exp(-x/T))
@@ -23,9 +23,9 @@ fig, axe = plt.subplots(1, 1)
 axe.plot(x1, y1, 'o', c='red', label = 'data')#'$k_{on}^{c}=2,k_{off}=    0.2$')
 axe.plot(x1, array_y_fit, '-', c='black', label = r'$P={}(1-exp(-t/{}))$'.format(param[0].round(3),param[1].round(3)))
 plt.xlabel("time", fontsize = 18)
-plt.ylabel("Percolatin Probability", fontsize = 18)
+plt.ylabel("number", fontsize = 18)
 axe.legend()
-fig.savefig("fit_num_kcon_ll.png")
+fig.savefig("fit_num_kcon_ss.png")
   
 #sns.pointplot(x=x1, y=y1, join=False)
 #sns.pointplot(x=x1, y=array_y_fit, markers="")
