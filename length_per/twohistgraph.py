0d2909ead3afd3a6fb9ee02ae5af7259476d1480
yuikaueda <yuika428@gmail.com>
2022-02-05 16:29:20 +0900

    n0205

--- length_per/twohistgraph.py
+++ length_per/twohistgraph.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-df = np.loadtxt('len_ramda6.dat')
+df = np.loadtxt('len_ramda10.dat')
 #df1 = df.round(2)
 
 x = np.linspace(0,1,100)
-y = 6*np.exp(-6*x)
+y = 10*np.exp(-10*x)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 ax2 = ax1.twinx()
 
-ret = ax1.hist(df, bins=10, range=(0.0,0.7), histtype='barstacked', ec='black', color='dodgerblue')
+ret = ax1.hist(df, bins=10, range=(0.0,0.7), histtype='barstacked', ec='black', color='magenta')
 #ret1 = list(ret)
 #ret1[0] = ret1[0]/491
 print(ret[1])
@@ -21,7 +21,7 @@ print(ret[1])
 #file.writelines(str(ret))
 #file.close()
 
-ax2.plot(x, y, '-', c='black', label =r'$y=6exp(-6x)$')
+ax2.plot(x, y, '-', c='black', label =r'$y=10exp(-10x)$')
 ax1.set_xlabel(r"Filament length", fontsize = 18)
 ax1.set_ylabel(r"Number", fontsize = 18)
 ax2.set_ylabel(r"PDF", fontsize = 18)
@@ -29,5 +29,5 @@ plt.xlim([0,0.7])
 ax1.set_ylim([0,200])
 ax2.set_ylim([0,10])
 ax2.legend(loc='best')
-fig.savefig("renew_color_twohist_len_ramda6.png")
+fig.savefig("re_color_twohist_len_ramda10.png")
 plt.show()
